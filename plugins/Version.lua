6a5292e3a092403ac710f6eabee1b5116d04c17e
Mr Ah S <mrahs@emeil.ir>
2016-01-15 18:27:38 +0330

    Rename version.lua to Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,20 @@
+do
+
+function run(msg, matches)
+  return 'SBSS Bot v1.5'.. [[ 
+  
+Powered by @Mr_Ah_S
+
+
+end
+
+return {
+  description = "Robot and Creator About", 
+  usage = "/ver : robot info",
+  patterns = {
+    "^[!/]ver$"
+  }, 
+  run = run 
+}
+
+end
